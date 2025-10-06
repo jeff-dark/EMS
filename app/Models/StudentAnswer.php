e3b229236a2969856a385af830406d18ba5683e9
jeff-dark <jeffhontez8501@gmail.com>
2025-10-06 17:01:11 +0000

    .

--- app/Models/StudentAnswer.php
+++ app/Models/StudentAnswer.php
@@ -27,24 +27,3 @@ class StudentAnswer extends Model
         return $this->belongsTo(Question::class);
     }
 }
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
-class StudentAnswer extends Model
-{
-    protected $fillable = ['exam_session_id', 'question_id', 'answer', 'awarded_points', 'teacher_feedback'];
-
-    public function examSession(): BelongsTo
-    {
-        return $this->belongsTo(ExamSession::class);
-    }
-
-    public function question(): BelongsTo
-    {
-        return $this->belongsTo(Question::class);
-    }
-}
