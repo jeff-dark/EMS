a4ead0f757a94e9d90318aea3797850962b82101
jeff-dark <jeffhontez8501@gmail.com>
2025-10-10 09:18:29 +0000

    .

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\{AdminController, Controller, CourseController, Dashboa
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('index');
-})->name('dashboard');
+    return Inertia::render('welcome');
+})->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
